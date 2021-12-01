1c8a95984dac75741a0b99a107979daff30d3f89
Vlogo11 <garaimark07@gmail.com>
2021-12-01 18:19:20 +0100

    BTC Mining Simulator 1.3.9

---
+++ BTC Mining Simulator/Assets/Scripts/CopyPaste.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CopyPaste : MonoBehaviour
+{
+    public InputField inputField;
+
+    public void CopyToClipboard()
+    {
+        TextEditor textEditor = new TextEditor();
+        textEditor.text = inputField.text;
+        textEditor.SelectAll();
+        textEditor.Copy();
+    }
+
+    public void PasteFromClipboard()
+    {
+        TextEditor textEditor = new TextEditor();
+        textEditor.multiline = true;
+        textEditor.Paste();
+        inputField.text = textEditor.text;
+    }
+}
---
+++ BTC Mining Simulator/Assets/Scripts/RandomGeneration.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class RandomGeneration : MonoBehaviour
+{
+    public GameObject TextBox;
+    public int TheNumber;
+
+    public void RandomGenerate()
+    {
+        TheNumber = Random.Range(100000, 999999);
+        TextBox.GetComponent<Text>().text = "" + TheNumber;
+    }
+}
